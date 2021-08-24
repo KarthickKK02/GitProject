57ca88abb9ba260eff87b70df5ed56950d804792
KarthickKK02 <itskarthi0210@gmail.com>
2021-08-24 21:21:59 +0530

    loadUrl done

--- src/org/GIT/LibGlobal.java
+++ src/org/GIT/LibGlobal.java
@@ -1,6 +1,12 @@
 package org.GIT;
 
 public class LibGlobal {
-
+	public static void loadUrl() {
+		System.out.println("loadUrl");
+		}
+	public static void main(String[] args) {
+		loadUrl();
+	}
 }
+
 
